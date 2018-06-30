07db6e2858a97005791debcaba0fd6478cce6e11
Jiawei Ren <renjiawei1997@gmail.com>
2018-06-30 23:02:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-import warnings
-
 from init_param import novelView
 from prepare_data import *
 
 warnings.filterwarnings("ignore")
-from train import load_networks, read_illum_images, evaluate_system, compute_psnr
+from train import load_networks, read_illum_images, evaluate_system, compute_psnr, param
 from skimage.color import rgb2hsv, hsv2rgb
 import pytorch_ssim
 from torch.autograd import Variable
@@ -22,14 +20,14 @@ def adjust_tone(input):
 
 
 def get_img_ind(inPos):
-    ind = int(inPos * (param.origAngRes - 1))
+    ind = int(round(inPos * (param.origAngRes - 1)))
     return ind
 
 
 def write_error(estimated, reference, resultPath):
     curPSNR = compute_psnr(estimated, reference)
-    estimated = Variable(torch.from_numpy(np.expand_dims(estimated,axis=3)).permute(3,2,0,1).float())
-    reference = Variable(torch.from_numpy(np.expand_dims(reference,axis=3)).permute(3,2,0,1).float())
+    estimated = Variable(estimated.unsqueeze(3).permute(3, 2, 0, 1))
+    reference = Variable(reference.unsqueeze(3).permute(3, 2, 0, 1))
     curSSIM = pytorch_ssim.ssim(estimated, reference).data[0]
 
     fid = open(resultPath + '/ObjectiveQuality.txt', 'w')
@@ -40,31 +38,40 @@ def write_error(estimated, reference, resultPath):
 
 def synthesize_novel_views(depth_net, color_net, inputLF, fullLF, resultPath):
     numNovelViews = len(novelView.Y)
-
-    # for vi in range(numNovelViews):
-    for vi in range(1):
+    if param.useGPU:
+        inputLF = torch.from_numpy(inputLF).cuda().float()
+        fullLF = torch.from_numpy(fullLF).cuda().float()
+    else:
+        inputLF = torch.from_numpy(inputLF).float()
+        fullLF = torch.from_numpy(fullLF).float()
+    for vi in range(numNovelViews):
         indY = get_img_ind(novelView.Y[vi])
         indX = get_img_ind(novelView.X[vi])
 
-        curRefPos = np.array([novelView.Y[vi], novelView.X[vi]])
-        curRefPos = np.expand_dims(curRefPos, axis=1)
-        curRefPos = torch.from_numpy(curRefPos).float()
+        curRefPos = np.array([[novelView.Y[vi]], [novelView.X[vi]]])
+
+        if param.useGPU:
+            curRefPos = torch.from_numpy(curRefPos).cuda().float()
+        else:
+            curRefPos = torch.from_numpy(curRefPos).float()
+
         # performs the whole process of extracting features, evaluating the
         # two sequential networks and generating the output synthesized image
-        print('View %02d of %02d' % (vi, numNovelViews))
+        print('View %02d of %02d' % (vi + 1, numNovelViews))
         print('**********************************')
         synthesizedView = evaluate_system(depth_net, color_net, images=inputLF, refPos=curRefPos)
 
         synthesizedView = synthesizedView[:, :, :, -1]
         # crop the result and reference images
+
         curEst = crop_img(synthesizedView, 10)
         curRef = crop_img(fullLF[:, :, :, indY, indX], param.depthBorder + param.colorBorder + 10)
-
-
         # write the numerical evaluation and the final image
-        if indY == 0 and indX == 0:
+        if indY == 4 and indX == 4:
             write_error(curEst, curRef, resultPath)
-        imwrite(resultPath + '/Images/' + ('%02d_%02d.png' % (indY, indX)), (adjust_tone(curEst) * 255).astype(int))
+        img = curEst.cpu().numpy()
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        imwrite(resultPath + '/Images/' + ('%02d_%02d.png' % (indY, indX)), (adjust_tone(img) * 255).astype(int))
 
 
 def test():
@@ -80,12 +87,12 @@ def test():
 
     for ns in range(numScenes):
         print('**********************************')
-        print('Working on the ' + sceneNames[ns][0:- 4] + 'dataset')
+        print('Working on the ' + sceneNames[ns][0:- 4] + ' dataset')
 
         resultPath = resultFolder + '/' + sceneNames[ns][0:- 4]
         make_dir(resultPath + '/Images')
 
-        print('Loading input light field ...',end = '')
+        print('Loading input light field ...', end='')
         [curFullLF, curInputLF] = read_illum_images(scenePaths[ns])
         print('Done')
         print('**********************************')
